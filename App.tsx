a8dbf4049ab285678dbb71eac4564677873816d1
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-04-11 17:00:10 -0300

    App title

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import { Button } from "./Button/Button";
 
 export default function App() {
@@ -13,44 +13,72 @@ export default function App() {
     }, 2000);
   }
   return (
-    <View style={styles.container}>
-      <Button
-        style={{ marginBottom: 20 }}
-        iconName="camera"
-        isLoading={loading}
-        onPress={onPressButton}
-        title="Tirar Foto"
-      />
-      <Button
-        disabled
-        style={{ marginBottom: 20 }}
-        isLoading={loading}
-        onPress={onPressButton}
-        title="Tirar Foto"
-      />
-      <Button
-        style={{ marginBottom: 20 }}
-        iconName="camera"
-        isLoading={true}
-        onPress={onPressButton}
-        title="Tirar Foto"
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 30,
+          marginTop: 16,
+          fontWeight: "bold",
+          color: "#550AB1",
+        }}
+      >
+        Button Completo e Customizável
+      </Text>
+      <View style={styles.container}>
+        <Button
+          style={{ marginBottom: 20 }}
+          iconName="camera"
+          isLoading={loading}
+          onPress={onPressButton}
+          title="Tirar Foto"
+        />
+        <Button
+          disabled
+          style={{ marginBottom: 20 }}
+          isLoading={loading}
+          onPress={onPressButton}
+          title="Primário desabilitado"
+        />
+        <Button
+          style={{ marginBottom: 20 }}
+          iconName="camera"
+          isLoading={true}
+          onPress={onPressButton}
+          title="Tirar Foto"
+        />
 
-      <Button
-        variant="outline"
-        style={{ marginBottom: 20 }}
-        iconName="bars"
-        onPress={onPressButton}
-        title="Outline Button"
-      />
-      <Button
-        disabled
-        style={{ marginBottom: 20 }}
-        variant="outline"
-        onPress={onPressButton}
-        title="Desabilitado"
-      />
-    </View>
+        <Button
+          variant="outline"
+          style={{ marginBottom: 20 }}
+          iconName="bars"
+          onPress={onPressButton}
+          title="Outline Button"
+        />
+        <Button
+          disabled
+          style={{ marginBottom: 20 }}
+          variant="outline"
+          onPress={onPressButton}
+          title="Desabilitado"
+        />
+        <Button
+          style={{ marginBottom: 20 }}
+          iconName="shoppingcart"
+          variant="blackFriday"
+          onPress={onPressButton}
+          title="Black Friday"
+        />
+        <Button
+          isLoading
+          style={{ marginBottom: 20 }}
+          iconName="shoppingcart"
+          variant="blackFriday"
+          onPress={onPressButton}
+          title="Black Friday"
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
